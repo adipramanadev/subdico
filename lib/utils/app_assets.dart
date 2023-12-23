e3c343ecad2c1f3d70289a63de357b774c60073a
adipramandev <herryprasetyo.dev@gmail.com>
2023-12-23 21:07:56 +0700

    addUtility

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,88 @@
+/// Icons
+const ic_google = "assets/ui_kits/home_service/icons/hs_ic_google.png";
+const ic_facebook = "assets/ui_kits/home_service/icons/hs_ic_facebook.png";
+const ic_twitter = "assets/ui_kits/home_service/icons/hs_ic_twitter.png";
+const ic_social_account =
+    "assets/ui_kits/home_service/icons/hs_ic_social_account.png";
+const ic_master_card =
+    "assets/ui_kits/home_service/icons/hs_ic_master_card.png";
+const ic_cleaning = "assets/ui_kits/home_service/icons/hs_ic_cleaning.png";
+const ic_edit = "assets/ui_kits/home_service/icons/hs_ic_edit.png";
+const ic_vehicle = "assets/ui_kits/home_service/icons/hs_ic_vehicle.png";
+const ic_india = "assets/ui_kits/home_service/icons/hs_ic_india.png";
+const ic_us = "assets/ui_kits/home_service/icons/hs_ic_us.png";
+const ic_uk = "assets/ui_kits/home_service/icons/hs_ic_uk.png";
+const ic_house = "assets/ui_kits/home_service/icons/hs_ic_house.png";
+const ic_office = "assets/ui_kits/home_service/icons/hs_ic_office.png";
+const ic_commercial = "assets/ui_kits/home_service/icons/hs_ic_commercial.png";
+const ic_labour = "assets/ui_kits/home_service/icons/hs_ic_labour.png";
+const ic_painting = "assets/ui_kits/home_service/icons/hs_ic_painting.png";
+const ic_chat = "assets/ui_kits/home_service/icons/hs_ic_chat.png";
+const ic_call = "assets/ui_kits/home_service/icons/hs_ic_call.png";
+const ic_email = "assets/ui_kits/home_service/icons/hs_ic_email.png";
+const ic_faq = "assets/ui_kits/home_service/icons/hs_ic_faq.png";
+const ic_reminder = "assets/ui_kits/home_service/icons/hs_ic_reminder.png";
+const ic_message = "assets/ui_kits/home_service/icons/hs_ic_message.png";
+const ic_order = "assets/ui_kits/home_service/icons/hs_ic_order.png";
+const ic_summer_offer =
+    "assets/ui_kits/home_service/icons/hs_ic_summer_offer.png";
+const office_1 = "assets/ui_kits/home_service/images/hs_office1.png";
+const office_2 = "assets/ui_kits/home_service/images/hs_office2.png";
+const office_3 = "assets/ui_kits/home_service/images/hs_office3.png";
+const ic_box = "assets/ui_kits/home_service/icons/hs_ic_box.png";
+const ic_working_person =
+    "assets/ui_kits/home_service/icons/hs_ic_working_person.png";
+const ic_clock = "assets/ui_kits/home_service/icons/hs_ic_clock.png";
+
+/// Images
+const house_1 = "assets/ui_kits/home_service/images/hs_house1.png";
+const house_2 = "assets/ui_kits/home_service/images/hs_house2.png";
+const house_3 = "assets/ui_kits/home_service/images/hs_house3.png";
+const crackers_img = "assets/ui_kits/home_service/images/hs_crackers.png";
+const profile = "assets/ui_kits/home_service/images/hs_profile.jpg";
+const map_img = "assets/ui_kits/home_service/images/hs_map.jpg";
+const smile_1 = "assets/ui_kits/home_service/images/hs_smile1.png";
+const smile_2 = "assets/ui_kits/home_service/images/hs_smile2.png";
+const smile_3 = "assets/ui_kits/home_service/images/hs_smile3.png";
+const smile_4 = "assets/ui_kits/home_service/images/hs_smile4.png";
+const smile_5 = "assets/ui_kits/home_service/images/hs_smile5.png";
+const offer_image1 = "assets/ui_kits/home_service/images/hs_offer_image1.jpg";
+const offer_image2 = "assets/ui_kits/home_service/images/hs_offer_image2.jpg";
+const offer_image3 = "assets/ui_kits/home_service/images/hs_offer_image3.jpg";
+const offer_image4 = "assets/ui_kits/home_service/images/hs_offer_image4.jpg";
+const offer_image5 = "assets/ui_kits/home_service/images/hs_offer_image5.jpg";
+const offer_image6 = "assets/ui_kits/home_service/images/hs_offer_image6.jpg";
+const offer_image7 = "assets/ui_kits/home_service/images/hs_offer_image7.jpg";
+const offer_image8 = "assets/ui_kits/home_service/images/hs_offer_image8.jpg";
+const cleaning_offer1 =
+    "assets/ui_kits/home_service/images/hs_cleaning_offer1.jpg";
+const cleaning_offer2 =
+    "assets/ui_kits/home_service/images/hs_cleaning_offer2.jpg";
+const labour_offer1 = "assets/ui_kits/home_service/images/hs_labour_offer1.jpg";
+const labour_offer2 = "assets/ui_kits/home_service/images/hs_labour_offer2.jpg";
+const painting_offer1 =
+    "assets/ui_kits/home_service/images/hs_painting_offer1.jpg";
+const painting_offer2 =
+    "assets/ui_kits/home_service/images/hs_painting_offer2.jpg";
+const vehicle_offer1 =
+    "assets/ui_kits/home_service/images/hs_vehicle_offer1.jpg";
+const vehicle_offer2 =
+    "assets/ui_kits/home_service/images/hs_vehicle_offer2.jpg";
+const mini_truck = "assets/ui_kits/home_service/images/hs_mini_truck.png";
+const car2 = "assets/ui_kits/home_service/images/hs_car2.png";
+const car = "assets/ui_kits/home_service/images/hs_car.png";
+const house2 = "assets/ui_kits/home_service/images/hs_house2.png";
+const sofa_image = "assets/ui_kits/home_service/images/hs_sofa_image.png";
+const worker = "assets/ui_kits/home_service/images/hs_worker.png";
+const bed = "assets/ui_kits/home_service/furniture_images/hs_bed.png";
+const sofa = "assets/ui_kits/home_service/furniture_images/hs_sofa.png";
+const chair = "assets/ui_kits/home_service/furniture_images/hs_chair.png";
+const almira = "assets/ui_kits/home_service/furniture_images/hs_almira.png";
+const ac = "assets/ui_kits/home_service/furniture_images/hs_ac.png";
+const fridge = "assets/ui_kits/home_service/furniture_images/hs_fridge.png";
+const oven = "assets/ui_kits/home_service/furniture_images/hs_oven.png";
+const tv = "assets/ui_kits/home_service/furniture_images/hs_tv.png";
+const wardrobe = "assets/ui_kits/home_service/furniture_images/hs_wardrobe.png";
+
+/// GIF
+const crackers = "assets/ui_kits/home_service/gif/hs_crackers.gif";
---
+++ lib/utils/app_data_helpers.dart
@@ -0,0 +1,383 @@
+import 'package:flutter/material.dart';
+import 'package:subdico/models/submodel.dart';
+import 'package:subdico/utils/app_assets.dart';
+import 'package:subdico/utils/app_colors.dart';
+
+import '../view/profile/faq_screen.dart';
+
+List<CountryModel> getCountryList() {
+  List<CountryModel> list = [];
+  list.add(CountryModel(title: "India (+91)", imagePath: ic_india));
+  list.add(CountryModel(title: "United States (+1)", imagePath: ic_us));
+  list.add(CountryModel(title: "United Kingdom (+92)", imagePath: ic_uk));
+  return list;
+}
+
+List<ShiftingServiceModel> getShiftingServiceList() {
+  List<ShiftingServiceModel> list = [];
+  list.add(ShiftingServiceModel(
+      title: 'House', imagePath: ic_house, color: Colors.red));
+  list.add(ShiftingServiceModel(
+      title: 'Office', imagePath: ic_office, color: secondaryColor));
+  list.add(ShiftingServiceModel(
+      title: 'Commercial', imagePath: ic_commercial, color: Colors.blue));
+  return list;
+}
+
+List<OfferAndNewsModel> getOfferAndNewsList() {
+  List<OfferAndNewsModel> list = [];
+  list.add(OfferAndNewsModel(
+      title: 'Trending', images: [offer_image1, offer_image2]));
+  list.add(OfferAndNewsModel(
+      title: 'Promotion', images: [offer_image3, offer_image4]));
+  list.add(OfferAndNewsModel(
+      title: 'Summer Offer', images: [offer_image5, offer_image6]));
+  list.add(OfferAndNewsModel(
+      title: 'New Collection', images: [offer_image7, offer_image8]));
+  return list;
+}
+
+List<HistoryModel> getHistoryList() {
+  List<HistoryModel> list = [];
+  list.add(HistoryModel(
+      title: 'House Shifting Service',
+      image: map_img,
+      address: '3329 joyce Street, PA, USA',
+      dateTime: 'Friday, May 11, 2021',
+      statusType: statusTypeActive));
+  list.add(HistoryModel(
+      title: 'Painting Service',
+      image: map_img,
+      address: '3329 joyce Street, PA, USA',
+      dateTime: 'Friday, May 11, 2021',
+      statusType: statusTypeCancelled));
+  list.add(HistoryModel(
+      title: 'Cleaning Service',
+      image: map_img,
+      address: '3329 joyce Street, PA, USA',
+      dateTime: 'Sunday, January 03, 2021',
+      statusType: statusTypeDone));
+  return list;
+}
+
+List<ShiftingServiceModel> getOtherServicesList() {
+  List<ShiftingServiceModel> list = [];
+  list.add(ShiftingServiceModel(
+      title: 'Cleaning',
+      color: greenColor,
+      imagePath: ic_cleaning,
+      newData: true,
+      widget: HSCleaningServiceScreen(title: 'Cleaning')));
+  list.add(ShiftingServiceModel(
+      title: 'Vehicle',
+      color: Colors.purple,
+      imagePath: ic_vehicle,
+      widget: HSVehicleServiceScreen()));
+  list.add(ShiftingServiceModel(
+      title: 'Labour Service',
+      color: Colors.blue,
+      imagePath: ic_labour,
+      widget: HSCleaningServiceScreen(title: 'Labour')));
+  list.add(ShiftingServiceModel(
+      title: 'Painting',
+      color: Colors.pink,
+      imagePath: ic_painting,
+      widget: HSCleaningServiceScreen(title: 'Painting')));
+  return list;
+}
+
+List<NotificationModel> getNotificationList() {
+  List<NotificationModel> list = [];
+  list.add(
+    NotificationModel(
+      title: 'Reminder',
+      subTitle: 'House Shifting - #2F33J scheduled Tomorrow.',
+      color: Colors.blue,
+      time: '13 min',
+      image: ic_reminder,
+    ),
+  );
+  list.add(
+    NotificationModel(
+      title: 'You have a new message',
+      subTitle:
+          '\"Hey! I looked your problem and it\'s fix now. can you confirm?\"',
+      color: secondaryColor,
+      time: '1 hr',
+      image: ic_message,
+    ),
+  );
+  list.add(
+    NotificationModel(
+      title: 'Order Confirmed',
+      subTitle: 'Your vehicle - Mini Van Order is successfully placed.',
+      color: greenColor,
+      time: '2 hr',
+      image: ic_order,
+    ),
+  );
+  list.add(
+    NotificationModel(
+      title: 'Summer Offer',
+      subTitle: '50% off on House Painting service until November 23rd.',
+      color: Colors.red,
+      time: '2 hr',
+      image: ic_summer_offer,
+    ),
+  );
+  return list;
+}
+
+List<SettingModel> getGeneralSettingList() {
+  List<SettingModel> list = [];
+  list.add(SettingModel(
+      iconData: Icons.payment,
+      title: 'Payment Methods',
+      subTitle: 'Add your credit & debit cards'));
+  list.add(SettingModel(
+      iconData: Icons.location_on,
+      title: 'Locations',
+      subTitle: 'Add your home & work locations'));
+  list.add(SettingModel(
+      iconData: Icons.person,
+      title: 'Add Social Account',
+      subTitle: 'Add Facebook, Instagram, Twitter etc',
+      widget: HSAddSocialAccountScreen()));
+  list.add(SettingModel(
+      iconData: Icons.wc,
+      title: 'Refer to Friends',
+      subTitle: 'Get \$10 for reffering friends'));
+  return list;
+}
+
+List<SettingModel> getNotificationSettingList() {
+  List<SettingModel> list = [];
+  list.add(SettingModel(
+      title: 'Push Notifications', subTitle: 'For daily updates and others'));
+  list.add(SettingModel(
+      title: 'Promotional Notifications', subTitle: 'New Campaign & offers'));
+  return list;
+}
+
+List<ContactUsModel> getContactUsList() {
+  List<ContactUsModel> list = [];
+  list.add(ContactUsModel(
+      image: ic_chat,
+      color: greenColor,
+      title: 'Support Chat',
+      subTitle: '24*7 Online Support'));
+  list.add(ContactUsModel(
+      image: ic_call,
+      color: secondaryColor,
+      title: 'Call Center',
+      subTitle: '24*7 Customer Service'));
+  list.add(ContactUsModel(
+      image: ic_email,
+      color: Colors.purple,
+      title: 'Email',
+      subTitle: 'admin@shify.com'));
+  list.add(ContactUsModel(
+      image: ic_faq,
+      color: Colors.yellow,
+      title: 'FAQ',
+      subTitle: '+50 Answers',
+      widget: FaqScreen()));
+  return list;
+}
+
+List<FaqModel> getGeneralFaqList() {
+  List<FaqModel> list = [];
+  list.add(
+      FaqModel(title: 'How to contact with riders ?', expandedText: 'N/A'));
+  list.add(FaqModel(
+      title: 'How to change my selected furniture ?', expandedText: 'N/A'));
+  list.add(FaqModel(title: 'What is cost of this item ?', expandedText: 'N/A'));
+  return list;
+}
+
+List<FaqModel> getContactFaqList() {
+  List<FaqModel> list = [];
+  list.add(FaqModel(
+      title: 'What is the customer care number ?', expandedText: 'N/A'));
+  list.add(FaqModel(
+      title: 'Can I cancel the order after one week ?',
+      expandedText:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      initiallyExpanded: true));
+  list.add(
+      FaqModel(title: 'How to call any service now ?', expandedText: 'N/A'));
+  return list;
+}
+
+List<HouseItemModel> getHouseSizeList() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(
+      image: house_1, title: '2 Bedrooms\n1 kitchen', color: secondaryColor));
+  list.add(HouseItemModel(
+      image: house_2,
+      title: '3 Bedrooms\n1 kitchen',
+      color: Colors.blueAccent));
+  list.add(HouseItemModel(
+      image: house_3,
+      title: '4 Bedrooms\n1 kitchen',
+      color: Colors.pinkAccent));
+  return list;
+}
+
+List<HouseItemModel> getOfficeSizeList() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(
+      image: office_1, title: 'Small Office', color: secondaryColor));
+  list.add(HouseItemModel(
+      image: office_2, title: 'Large Office', color: Colors.blueAccent));
+  list.add(HouseItemModel(
+      image: office_3, title: 'Multiple Office', color: Colors.pinkAccent));
+  return list;
+}
+
+List<HouseItemModel> getFurnitureList() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(title: 'Bed', color: Colors.purple, image: bed));
+  list.add(HouseItemModel(title: 'Sofa', color: secondaryColor, image: sofa));
+  list.add(
+      HouseItemModel(title: 'Chair', color: Colors.blueAccent, image: chair));
+  list.add(
+      HouseItemModel(title: 'Almira', color: Colors.orange, image: almira));
+  list.add(HouseItemModel(title: 'AC', color: Colors.red, image: ac));
+  list.add(HouseItemModel(title: 'Fridge', color: Colors.blue, image: fridge));
+  list.add(HouseItemModel(title: 'Oven', color: Colors.yellow, image: oven));
+  list.add(HouseItemModel(title: 'TV', color: Colors.green, image: tv));
+  list.add(
+      HouseItemModel(title: 'Wardrobe', color: Colors.pink, image: wardrobe));
+  return list;
+}
+
+List<HouseItemModel> getOfficeItemList() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(title: 'Bed', color: Colors.purple, image: bed));
+  list.add(HouseItemModel(title: 'Sofa', color: secondaryColor, image: sofa));
+  list.add(
+      HouseItemModel(title: 'Chair', color: Colors.blueAccent, image: chair));
+  list.add(HouseItemModel(title: 'AC', color: Colors.red, image: ac));
+  return list;
+}
+
+List<HouseItemModel> getOtherItems() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(
+      title: 'Packed Boxes',
+      subTitle: 'Weight below 10 kg',
+      image: ic_box,
+      count: 0));
+  list.add(HouseItemModel(
+      title: 'Workers',
+      subTitle: 'Regular cost is \$5/hr.Total cost will be calculated later.',
+      image: ic_working_person,
+      count: 0));
+  list.add(HouseItemModel(
+      title: 'Electrician',
+      subTitle: 'Regular cost is \$10/hr.Total cost will be calculated later.',
+      image: ic_working_person,
+      count: 0));
+  return list;
+}
+
+List<HouseItemModel> getCleaningServiceItems() {
+  List<HouseItemModel> list = [];
+  list.add(HouseItemModel(
+      title: 'Required Person',
+      subTitle: 'Regular cost is \$10/hr.Total cost will be calculated later.',
+      image: ic_clock,
+      count: 0));
+  list.add(HouseItemModel(
+      title: 'Working Hour',
+      subTitle: 'Cost will be increase after  2hrs of work.',
+      image: ic_working_person,
+      count: 2));
+  return list;
+}
+
+List<String> getCleaningOfferImages() {
+  List<String> list = [cleaning_offer1, cleaning_offer2];
+  return list;
+}
+
+List<String> getLabourOfferImages() {
+  List<String> list = [labour_offer1, labour_offer2];
+  return list;
+}
+
+List<String> getPaintingOfferImages() {
+  List<String> list = [painting_offer1, painting_offer2];
+  return list;
+}
+
+List<String> getVehicleOfferImages() {
+  List<String> list = [vehicle_offer1, vehicle_offer2];
+  return list;
+}
+
+List<String> getPickTimeList() {
+  List<String> list = [
+    '07:00 AM',
+    '11:00 AM',
+    '12:00 PM',
+    '02:00 PM',
+    '04:00 PM',
+    '06:00 PM'
+  ];
+  return list;
+}
+
+List<VehicleItemModel> getPickupVehicleList() {
+  List<VehicleItemModel> list = [];
+  list.add(VehicleItemModel(
+      title: 'Mini Truck', image: mini_truck, capacity: '18 Ton'));
+  list.add(VehicleItemModel(title: 'Pickup', image: car2, capacity: '12 Ton'));
+  list.add(VehicleItemModel(title: 'Large', image: car, capacity: '5 Ton'));
+  return list;
+}
+
+List<VehicleItemModel> getVehicleItemList() {
+  List<VehicleItemModel> list = [];
+  list.add(VehicleItemModel(
+      title: 'Mini Truck', image: mini_truck, capacity: '18 Ton. 9 feet'));
+  list.add(VehicleItemModel(
+      title: 'Pickup', image: car2, capacity: '12 Ton, 7 feet'));
+  list.add(
+      VehicleItemModel(title: 'Large', image: car, capacity: '5 Ton, 14 feet'));
+  return list;
+}
+
+List<HouseItemModel> getOrderDetailsList() {
+  List<HouseItemModel> list = [];
+  list.add(
+    HouseItemModel(
+      image: house2,
+      color: Colors.blueAccent,
+      title: '3 Bedrooms, 1 kitchen',
+      subTitle: '+\$5 for baby room',
+      dollarPrice: 22,
+    ),
+  );
+  var sofa_image2 = sofa_image;
+  list.add(
+    HouseItemModel(
+      image: sofa_image2,
+      title: '8 Furniture, 6 Boxes',
+      subTitle: '+\$5 for additional box',
+      dollarPrice: 30,
+      color: Colors.purple,
+    ),
+  );
+  list.add(
+    HouseItemModel(
+      image: worker,
+      color: Colors.pinkAccent,
+      title: '3 Worker, 1 Electrician',
+      subTitle: '+\$5 for additional person',
+      dollarPrice: 15,
+    ),
+  );
+  return list;
+}
